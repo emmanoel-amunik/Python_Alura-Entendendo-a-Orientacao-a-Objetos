b0644e8e0690109f92876ed231b8facea43b0551
emmanoel-amunik <emmanoelamunik@hotmail.com>
2023-12-14 22:42:39 -0300

    04. Encapsulamento

--- account.py
+++ account.py
@@ -3,16 +3,20 @@ class Account:
 
     def __init__(self, number, owner, balance, limit):
         print(f"Building object... {self}")
-        self.number = number
-        self.owner = owner
-        self.balance = balance
-        self.limit = limit
+        self.__number = number
+        self.__owner = owner
+        self.__balance = balance
+        self.__limit = limit
 
     def statement(self):
-        print(f"The Balance is {self.balance} of the owner {self.owner}.")
+        print(f"The Balance is {self.__balance} of the owner {self.__owner}.")
 
     def deposit(self, value):
-        self.balance += value
+        self.__balance += value
 
     def withdraw(self, value):
-        self.balance -= value
+        self.__balance -= value
+
+    def transfer(self, value, destination):
+        self.withdraw(value)
+        destination.deposit(value)
